27215be23092ef5e0456391ace81494e1a43614c
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-01-09 11:56:07 +0100

    functions of expanding and collapsing descriptions

---
+++ faq-accordion-main/app.js
@@ -0,0 +1,39 @@
+function question1() {
+  if (document.getElementById("answer1").style.display === "none") {
+    document.getElementById("answer1").style.display = "block";
+    document.getElementById("icon1").src = "assets/images/icon-minus.svg";
+  } else {
+    document.getElementById("answer1").style.display = "none";
+    document.getElementById("icon1").src = "assets/images/icon-plus.svg";
+  }
+}
+
+function question2() {
+  if (document.getElementById("answer2").style.display === "none") {
+    document.getElementById("answer2").style.display = "block";
+    document.getElementById("icon2").src = "assets/images/icon-minus.svg";
+  } else {
+    document.getElementById("answer2").style.display = "none";
+    document.getElementById("icon2").src = "assets/images/icon-plus.svg";
+  }
+}
+
+function question3() {
+  if (document.getElementById("answer3").style.display === "none") {
+    document.getElementById("answer3").style.display = "block";
+    document.getElementById("icon3").src = "assets/images/icon-minus.svg";
+  } else {
+    document.getElementById("answer3").style.display = "none";
+    document.getElementById("icon3").src = "assets/images/icon-plus.svg";
+  }
+}
+
+function question4() {
+  if (document.getElementById("answer4").style.display === "none") {
+    document.getElementById("answer4").style.display = "block";
+    document.getElementById("icon4").src = "assets/images/icon-minus.svg";
+  } else {
+    document.getElementById("answer4").style.display = "none";
+    document.getElementById("icon4").src = "assets/images/icon-plus.svg";
+  }
+}
